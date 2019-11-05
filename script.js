66e78ece71c624b0a81e7dd8ae86ad14d5797ab4
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-11-05 09:32:16 +0100

    dag 1

---
+++ script.js
@@ -0,0 +1,15 @@
+function verander1 (){
+    document.getElementById("ikzelf2").style.display="none";
+    document.getElementById("paard1").style.display="block";
+
+}
+function verander2(){
+    document.getElementById("paard1").style.display="none";
+    document.getElementById("paard2").style.display="block";
+
+}
+
+function verander3(){
+    document.getElementById("paard2").style.display="none";
+    document.getElementById("ikzelf2").style.display="block";
+}
